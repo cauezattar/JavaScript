f50d487ceb9aa68595064b7ccb195319d7dffee7
cauezattar <cauezattar@gmail.com>
2026-09-04 17:37:52 -0300

    add Declaração-const.js

---
+++ Declaração-const.js
@@ -0,0 +1,11 @@
+function exemploConst() {
+    const x = 10;
+    console.log(x); // 10
+    // x = 20; // Isso causará um erro porque x foi declarado como const
+    if (true) {
+        const y = 30;
+        console.log(y); // 30
+    }
+    // console.log(y); // Isso causará um erro porque y não existe fora do bloco
+}
+exemploLet();
