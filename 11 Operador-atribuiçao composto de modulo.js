a83127cbcab54b4f2f0ae7c76e3d1c475b31e6a6
cauezattar <cauezattar@gmail.com>
2026-09-11 17:28:07 -0300

    11 Operador-atribuiçao composto de modulo.js

---
+++ 11 Operador-atribuiçao composto de modulo.js
@@ -0,0 +1,7 @@
+let totalDoces = 10;
+let crianças = 3;
+
+// Queremos saber quantos doces sobram apos dividir igualmente
+totalDoces %= crianças;
+
+console.log(totalDoces); // 1 -> Pois 10 dividindo por 3 sobra 1
